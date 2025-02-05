3508969fef488b7daf036b5c0df1d6e838db7105
Hogbonouto Zinsou Kentin <66114703+kentinHogbonouto@users.noreply.github.com>
2025-02-05 15:43:37 +0100

    feat: setted up grid template. (#17)
    Co-authored-by: Nebur242 <57473126+Nebur242@users.noreply.github.com>

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -1,11 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import enCart from './locales/en/cart.json';
+import frCart from './locales/fr/cart.json';
+
 import enCommon from './locales/en/common.json';
 import frCommon from './locales/fr/common.json';
 
 import enDashboardTranslation from './locales/en/dashboard.json';
 import frDashboardTranslation from './locales/fr/dashboard.json';
+
+import enFooter from './locales/en/footer.json';
+import frFooter from './locales/fr/footer.json';
 
 import enHero from './locales/en/hero.json';
 import frHero from './locales/fr/hero.json';
@@ -15,13 +21,17 @@ const lng = localStorage.getItem('language') || 'fr';
 i18n.use(initReactI18next).init({
   resources: {
     en: {
+      cart: enCart,
       common: enCommon,
       dashboard: enDashboardTranslation,
+      footer: enFooter,
       hero: enHero,
     },
     fr: {
+      cart: frCart,
       common: frCommon,
       dashboard: frDashboardTranslation,
+      footer: frFooter,
       hero: frHero,
     },
   },
